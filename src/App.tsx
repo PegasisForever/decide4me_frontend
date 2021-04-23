ab9aadd686aef33d9a8ab1f1e374356b61c3f90c
pegasis <admin@pegasis.site>
2021-04-23 19:19:35 -0400

    add post component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import {Post, TextPostData} from './model/post'
+import {PostComponent} from './components/post_component'
+
+const testPost = new Post(
+  'user_id',
+  'Title',
+  'desc desc desc desc desc desc desc desc ',
+  new Date(),
+  55,
+  10,
+  new TextPostData(
+    new Map<string, number>([['aaaaa', 1], ['bbbbbbbb', 2], ['cccccccccc', 3]]),
+  ),
+  null,
+)
 
 function App() {
   return (
     <div className="App">
-     awa
+      <PostComponent post={testPost}/>
     </div>
   )
 }
---
+++ src/components/post_component.tsx
@@ -0,0 +1,31 @@
+import {Post} from '../model/post'
+import React, {ReactNode} from 'react'
+import {Box} from '@material-ui/core'
+
+export function PostComponent({post}: { post: Post }) {
+  return <Box border={'2px solid black'}>
+    <h3>{post.title}</h3>
+    <p>{post.time.toDateString()}</p>
+    <p>{post.description}</p>
+    {post.textData ? <TextPostPart post={post}/> : <ImagePostPart post={post}/>}
+    <p>{post.views} views</p>
+  </Box>
+}
+
+function TextPostPart({post}: { post: Post }) {
+  const choiceLis: Array<ReactNode> = []
+  post.textData!.choices.forEach((votes, choice) => {
+    choiceLis.push(<li>{choice}: {votes}</li>)
+  })
+  return <div>
+    <ol>
+      {choiceLis}
+    </ol>
+  </div>
+}
+
+function ImagePostPart({post}: { post: Post }) {
+  return <div>
+    image part
+  </div>
+}
