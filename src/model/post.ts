6d7c397f7a4678d2df8a536cd702d744f28f015b
pegasis <admin@pegasis.site>
2021-04-23 19:19:23 -0400

    add model

---
+++ src/model/post.ts
@@ -0,0 +1,28 @@
+export class Post {
+  constructor(
+    public userID: string,
+    public title: string,
+    public description: string,
+    public time: Date,
+    public views: number,
+    public targetVotes: number,
+    public textData: TextPostData|null,
+    public imageData: ImagePostData|null,
+  ) {
+  }
+}
+
+export class TextPostData {
+  constructor(
+    public choices: Map<string, number>,
+  ) {
+  }
+}
+
+export class ImagePostData {
+  constructor(
+    public imageUrl: string,
+    public choices: Array<{ x: number, y: number }>,
+  ) {
+  }
+}
